9add338ef3e4a0c5e50237395a71a1fac3e6f0d2
Alga Saputra <68761934+AlgaSaputra@users.noreply.github.com>
2022-09-26 16:54:49 +0700

    Update header.php

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -43,7 +43,7 @@
     <header id="header" class="header fixed-top d-flex align-items-center">
 
         <div class="d-flex align-items-center justify-content-between">
-            <a href="index.html" class="logo d-flex align-items-center">
+            <a href="<?= base_url(); ?>index.php/" class="logo d-flex align-items-center">
                 <img src="<?= base_url('public'); ?>/assets/img/logo.png" alt="">
                 <span class="d-none d-lg-block">Koperasi</span>
             </a>
