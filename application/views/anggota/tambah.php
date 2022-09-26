2ce15285ac29c88bcbd10453c6f16c432f69dded
Alga Saputra <68761934+AlgaSaputra@users.noreply.github.com>
2022-09-26 16:10:13 +0700

    perbaikan menu tambah

--- application/views/anggota/tambah.php
+++ application/views/anggota/tambah.php
@@ -1,15 +1,25 @@
+<?php
+$this->load->view('templates/header');
+$this->load->view('templates/sidebar');
+?>
+
 <form action="" method="POST">
-    <input type="text" name="URUT_ANG" class="form-control" id="URUT_ANG" placeholder="No urut anggota" /> <br>
+    <input type="text" name="URUT_ANG" class="form-control" id="URUT_ANG" placeholder="Nomor Urut Anggota" /> <br>
     <small class="form-text text-danger"><?= form_error('URUT_ANG'); ?></small>
 
-    <input type="text" name="NAMA_ANG" class="form-control" id="NAMA_ANG" placeholder="Nama anggota" /> <br>
+    <input type="text" name="NAMA_ANG" class="form-control" id="NAMA_ANG" placeholder="Nama Anggota" /> <br>
     <small class="form-text text-danger"><?= form_error('NAMA_ANG'); ?></small>
     
-    <input type="text" name="KODE_INS" class="form-control" id="KODE_INS" placeholder="Kode instansi" /> <br>
+    <input type="text" name="KODE_INS" class="form-control" id="KODE_INS" placeholder="Kode Instansi" /> <br>
     <small class="form-text text-danger"><?= form_error('KODE_INS'); ?></small>
     
-    <input type="text" name="NAMA_INS" class="form-control" id="NAMA_INS" placeholder="Nama instansi" /> <br>
+    <input type="text" name="NAMA_INS" class="form-control" id="NAMA_INS" placeholder="Nama Instansi" /> <br>
     <small class="form-text text-danger"><?= form_error('NAMA_INS'); ?></small>
 
-    <button type="submit" name="tambah" class="btn btn-primary">simpan</button>
+    <button type="submit" name="tambah" class="btn btn-primary">Simpan</button>
 </form>
+
+
+<?php
+$this->load->view('templates/footer');
+?>
--- application/views/instansi/tambah.php
+++ application/views/instansi/tambah.php
@@ -5,19 +5,19 @@ $this->load->view('templates/sidebar');
 
 
 <form action="" method="POST">
-    <input type="text" name="KODE_INS" class="form-control" id="KODE_INS" placeholder="kode instansi" />
+    <input type="text" name="KODE_INS" class="form-control" id="KODE_INS" placeholder="Kode Instansi" /> <br>
     <small class="form-text text-danger"><?= form_error('KODE_INS'); ?></small>
 
-    <input type="text" name="NAMA_INS" class="form-control" id="NAMA_INS" placeholder="nama instansi" />
+    <input type="text" name="NAMA_INS" class="form-control" id="NAMA_INS" placeholder="Nama Instansi" /> <br>
     <small class="form-text text-danger"><?= form_error('NAMA_INS'); ?></small>
 
-    <textarea class="form-control" name="ALM_INS" placeholder="alamat instansi"></textarea>
+    <textarea class="form-control" name="ALM_INS" placeholder="Alamat Instansi"></textarea> <br>
     <small class="form-text text-danger"><?= form_error('ALM_INS'); ?></small>
     
-    <input type="text" name="TLP_INS" class="form-control" id="TLP_INS" placeholder="no telepon" />
+    <input type="text" name="TLP_INS" class="form-control" id="TLP_INS" placeholder="Nomor Telepon" /> <br>
     <small class="form-text text-danger"><?= form_error('TLP_INS'); ?></small>
 
-    <button type="submit" name="tambah" class="btn btn-primary">simpan</button>
+    <button type="submit" name="tambah" class="btn btn-primary">Simpan</button>
 </form>
 
 
