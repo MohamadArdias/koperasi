71c7169738786b87aa8a45298d69bc312e7fb136
Snoopy <ardamsing@gmail.com>
2022-09-27 19:48:25 +0700

    Update edit.php

--- application/views/anggota/edit.php
+++ application/views/anggota/edit.php
@@ -1,14 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
-</head>
 <?php
 $this->load->view('templates/header');
 $this->load->view('templates/sidebar');
@@ -97,4 +86,3 @@ $this->load->view('templates/sidebar');
     </script>
 </body>
 
-</html>
