d7ce7fa14af72f431c992d6136a53ff1c40b9ecd
Snoopy <ardamsing@gmail.com>
2022-09-27 21:38:29 +0700

    Update tambah.php

--- application/views/anggota/tambah.php
+++ application/views/anggota/tambah.php
@@ -7,7 +7,7 @@ $this->load->view('templates/sidebar');
     <form action="" method="POST">
         <div class="card-body row mt-4">
             <div class="form-group row mb-2">
-                <label for="nama" class="col-sm-3 text-end control-label col-form-label">No Urut Anggota</label>
+                <label for="nama" class="col-sm-2 text-end control-label col-form-label">No Urut Anggota</label>
                 <div class="col-sm-9">
                     <input type="text" name="URUT_ANG" class="form-control" id="URUT_ANG" placeholder="-" />
                     <small class="form-text text-danger"><?= form_error('URUT_ANG'); ?></small>
@@ -15,7 +15,7 @@ $this->load->view('templates/sidebar');
             </div>
 
             <div class="form-group row mb-2">
-                <label for="nama" class="col-sm-3 text-end control-label col-form-label">Kode Anggota</label>
+                <label for="nama" class="col-sm-2 text-end control-label col-form-label">Kode Anggota</label>
                 <div class="col-sm-9">
                     <input type="text" name="KODE_ANG" class="form-control" id="KODE_ANG" placeholder="-" />
                     <small class="form-text text-danger"><?= form_error('KODE_ANG'); ?></small>
@@ -23,7 +23,7 @@ $this->load->view('templates/sidebar');
             </div>
 
             <div class="form-group row mb-2">
-                <label for="nama" class="col-sm-3 text-end control-label col-form-label">Nama</label>
+                <label for="nama" class="col-sm-2 text-end control-label col-form-label">Nama</label>
                 <div class="col-sm-9">
                     <input type="text" name="NAMA_ANG" class="form-control" id="NAMA_ANG" />
                     <small class="form-text text-danger"><?= form_error('NAMA_ANG'); ?></small>
@@ -31,7 +31,7 @@ $this->load->view('templates/sidebar');
             </div>
 
             <div class="form-group row mb-2">
-                <label for="kdins" class="col-sm-3 text-end control-label col-form-label">Kode Instansi</label>
+                <label for="kdins" class="col-sm-2 text-end control-label col-form-label">Kode Instansi</label>
                 <div class="col-sm-9">
                     <input type="text" name="KODE_INS" class="form-control" id="KODE_INS" />
                     <small class="form-text text-danger"><?= form_error('KODE_INS'); ?></small>
@@ -39,7 +39,7 @@ $this->load->view('templates/sidebar');
             </div>
 
             <div class="form-group row mb-2">
-                <label for="namains" class="col-sm-3 text-end control-label col-form-label">Nama Instansi</label>
+                <label for="namains" class="col-sm-2 text-end control-label col-form-label">Nama Instansi</label>
                 <div class="col-sm-9">
                     <input type="text" name="NAMA_INS" class="form-control" id="NAMA_INS" placeholder="-" />
                     <small class="form-text text-danger"><?= form_error('NAMA_INS'); ?></small>
@@ -47,7 +47,7 @@ $this->load->view('templates/sidebar');
             </div>
 
             <div class="form-group row mb-2">
-                <label for="ttl" class="col-sm-3 text-end control-label col-form-label"> Tempat Tanggal Lahir</label>
+                <label for="ttl" class="col-sm-2 text-end control-label col-form-label">Tanggal Lahir</label>
                 <div class="col-sm-9">
                     <div class="input-group">
                         <i class="fa-solid fa-calendar-days input-group-text "></i>
@@ -58,7 +58,7 @@ $this->load->view('templates/sidebar');
             </div>
 
             <div class="form-group row mb-2">
-                <label for="alamat" class="col-sm-3 text-end control-label col-form-label">Alamat</label>
+                <label for="alamat" class="col-sm-2 text-end control-label col-form-label">Alamat</label>
                 <div class="col-sm-9">
                     <input type="text" class="form-control" id="ALM_ANG" placeholder="-" name="ALM_ANG">
                     <small class="form-text text-danger"><?= form_error('ALM_ANG') ?></small>
@@ -66,7 +66,7 @@ $this->load->view('templates/sidebar');
             </div>
 
             <div class="form-group row mb-2">
-                <label for="In" class="col-sm-3 text-end control-label col-form-label">Tanggal Masuk</label>
+                <label for="In" class="col-sm-2 text-end control-label col-form-label">Tanggal Masuk</label>
                 <div class="col-sm-9">
                     <div class="input-group">
                         <i class="fa-solid fa-calendar-days input-group-text"></i>
@@ -77,7 +77,7 @@ $this->load->view('templates/sidebar');
             </div>
 
             <div class="form-group row mb-2">
-                <label for="Out" class="col-sm-3 text-end control-label col-form-label">Tanggal Keluar</label>
+                <label for="Out" class="col-sm-2 text-end control-label col-form-label">Tanggal Keluar</label>
                 <div class="col-sm-9">
                     <div class="input-group">
                         <i class="fa-solid fa-calendar-days input-group-text"></i>
@@ -88,12 +88,12 @@ $this->load->view('templates/sidebar');
             </div>
 
             <div class="form-group row mb-2">
-                <label for="Golongan" class="col-sm-3 text-end control-label col-form-label">Golongan</label>
+                <label for="Golongan" class="col-sm-2 text-end control-label col-form-label">Golongan</label>
                 <div class="col-sm-9">
                     <input type="text" class="form-control" id="GOL" placeholder="-" name="GOL">
                     <small class="form-text text-danger"><?= form_error('GOL') ?></small>
                 </div>
-                <div class="col-sm-3 text-end mt-3 ">
+                <div class="col-sm-2 text-end mt-3 ">
                     <button type="submit" name="edit" class="btn btn-primary">simpan</button>
                 </div>
             </div>
