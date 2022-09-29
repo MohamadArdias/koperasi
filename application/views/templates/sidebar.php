77ac55d5d1fdf219e8667e3e4f84b3db036288fb
Snoopy <ardamsing@gmail.com>
2022-09-29 10:00:31 +0700

    Update sidebar.php

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -9,14 +9,14 @@
                   <span>Dashboard</span>
               </a>
           </li>
-          
+
           <li class="nav-item">
               <a class="nav-link collapsed" href="<?= base_url(); ?>index.php/">
                   <i class="bi bi-grid"></i>
                   <span>Pengguna</span>
               </a>
           </li>
-          
+
           <li class="nav-item">
               <a class="nav-link collapsed" href="<?= base_url(); ?>index.php/Anggota">
                   <i class="bi bi-grid"></i>
@@ -28,6 +28,13 @@
               <a class="nav-link collapsed" href="<?= base_url(); ?>index.php/Instansi">
                   <i class="bi bi-grid"></i>
                   <span>Instansi</span>
+              </a>
+          </li>
+
+          <li class="nav-item">
+              <a class="nav-link collapsed" href="<?= base_url(); ?>index.php/Laporan">
+                  <i class="bi bi-grid"></i>
+                  <span>Laporan</span>
               </a>
           </li>
 
