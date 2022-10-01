90ab2a507614d2d021b1865e418920e32976f417
Snoopy <ardamsing@gmail.com>
2022-10-01 16:42:52 +0700

    Update detail.php

--- application/views/anggota/detail.php
+++ application/views/anggota/detail.php
@@ -3,110 +3,120 @@ $this->load->view('templates/header');
 $this->load->view('templates/sidebar');
 ?>
 
-<!-- <body> -->
 <div class="card">
-    <div class="card-body row mt-4">
-        <div class="form-group row mb-2 ms-5">
-            <label for="nama" class="from-label">Kode Anggota</label>
+    <div class="card-body mt-4">
+        <div class="form-group row mb-2">
+            <label for="nama" class="col-sm-2 text-end control-label col-form-label">Kode Urut</label>
             <div class="col-sm-9">
                 <div class="input-group">
                     <i class="bi-person-fill input-group-text"></i>
-                    <input type="text" name="KODE_ANG" class="form-control" id="" value="<?= $anggota['KODE_ANG']; ?>" readonly>
-                    <br>
+                    <input type="text" name="URUT_ANG" class="form-control" id="URUT_ANG" value="<?= $anggota['URUT_ANG']; ?>" />
                 </div>
+                <small class="form-text text-danger"><?= form_error('KODE_ANG'); ?></small>
             </div>
         </div>
-        <div class="form-group row mb-2 ms-5">
-            <label for="ttl" class="form-label">Nama</label>
+
+        <div class="form-group row mb-2">
+            <label for="nama" class="col-sm-2 text-end control-label col-form-label">Nama</label>
             <div class="col-sm-9">
                 <div class="input-group">
                     <i class="bi-person-fill input-group-text"></i>
-                    <input type="text" class="form-control" id="" name="birthday" value="<?= $anggota['NAMA_ANG']; ?>" readonly>
-                    <br>
+                    <input type="text" name="NAMA_ANG" class="form-control" id="NAMA_ANG" value="<?= $anggota['NAMA_ANG']; ?>" />
                 </div>
+                <small class="form-text text-danger"><?= form_error('NAMA_ANG'); ?></small>
             </div>
         </div>
-        <div class="form-group row mb-2 ms-5">
-            <label for="kredit" class="form-label">Limit Kredit </label>
-            <div class="col-sm-9">
-                <div class="input-group mb-3">
-                    <span class="input-group-text">Rp.</span>
-                    <input type="text" class="form-control" id="" value="" required readonly>
-                </div>
-            </div>
-        </div>
-        <div class="form-group row mb-2 ms-5">
-            <label for="nama" class="from-label">Kode Instansi</label>
+
+        <div class="form-group row mb-2">
+            <label for="kdins" class="col-sm-2 text-end control-label col-form-label">Kode Instansi</label>
             <div class="col-sm-9">
                 <div class="input-group">
                     <i class="bi-briefcase-fill input-group-text"></i>
-                    <input type="text" class="form-control" id=" value=" value="<?= $anggota['KODE_INS']; ?>" readonly>
-                    <br>
+                    <input type="text" name="KODE_INS" class="form-control" id="KODE_INS" value="<?= $anggota['KODE_INS']; ?>" />
                 </div>
+                <small class="form-text text-danger"><?= form_error('KODE_INS'); ?></small>
             </div>
         </div>
-        <div class="form-group row mb-2 ms-5">
-            <label for="Instansi" class="from-label">Nama Instansi</label>
+
+        <div class="form-group row mb-2">
+            <label for="namains" class="col-sm-2 text-end control-label col-form-label">Nama Instansi</label>
             <div class="col-sm-9">
                 <div class="input-group">
                     <i class="bi-briefcase-fill input-group-text"></i>
-                    <input type="text" class="form-control" id="validationDefault01" value="<?= $anggota['NAMA_INS']; ?>" readonly>
-                    <br>
+                    <input type="text" name="NAMA_INS" class="form-control" id="NAMA_INS" placeholder="-" value="<?= $anggota['NAMA_INS']; ?>" />
                 </div>
+                <small class="form-text text-danger"><?= form_error('NAMA_INS'); ?></small>
             </div>
         </div>
-        <div class="form-group row mb-2 ms-5">
-            <label for="ttl" class="form-label">Tempat Tanggal Lahir</label>
+
+        <div class="form-group row mb-2">
+            <label for="ttl" class="col-sm-2 text-end control-label col-form-label">Tanggal Lahir</label>
             <div class="col-sm-9">
                 <div class="input-group ">
                     <i class="bi-calendar3 input-group-text "></i>
-                    <input type="text" class="form-control" id="" name="birthday" value="<?= $anggota['TLHR_ANG']; ?>" readonly>
-                    <br>
+                    <input type="text" class="form-control" id="TLHR_ANG" name="TLHR_ANG" placeholder="-" value="<?= $anggota['TLHR_ANG']; ?>">
                 </div>
+                <small class="form-text text-danger"><?= form_error('TLHR_ANG') ?></small>
             </div>
         </div>
-        <div class="form-group row mb-2 ms-5">
-            <label for="alamat" class="from-label">Alamat</label>
+
+        <div class="form-group row mb-2">
+            <label for="alamat" class="col-sm-2 text-end control-label col-form-label">Alamat</label>
             <div class="col-sm-9">
                 <div class="input-group">
                     <i class="bi-geo-alt-fill input-group-text "></i>
-                    <textarea class="form-control" id="floatingTextarea2" placeholder="<?= $anggota['ALM_ANG']; ?>" style="height: 100px" readonly></textarea>
-                    <br>
+                    <input type="text" class="form-control" id="ALM_ANG" placeholder="-" name="ALM_ANG" value="<?= $anggota['ALM_ANG']; ?>">
                 </div>
             </div>
+            <small class="form-text text-danger"><?= form_error('ALM_ANG') ?></small>
         </div>
-        <div class="form-group row mb-2 ms-5">
-            <label for="In" class="from-label">Tanggal Masuk</label>
+
+        <div class="form-group row mb-2">
+            <label for="In" class="col-sm-2 text-end control-label col-form-label">Tanggal Masuk</label>
             <div class="col-sm-9">
                 <div class="input-group ">
-                    <i class="bi-calendar3 input-group-text "></i>
-                    <input type="text" class="form-control" id="validationDefault01" value="<?= $anggota['TGLM_ANG']; ?>" required readonly>
-                    <br>
+                    <i class="bi-calendar3 input-group-text"></i>
+                    <input type="text" class="form-control" id="TGLM_ANG" placeholder="-" name="TGLM_ANG" value="<?= $anggota['TGLM_ANG']; ?>">
                 </div>
+                <small class="form-text text-danger"><?= form_error('TGLM_ANG') ?></small>
             </div>
         </div>
-        <div class="form-group row mb-2 ms-5">
-            <label for="Out" class="from-label">Tanggal Keluar</label>
+
+        <div class="form-group row mb-2">
+            <label for="Out" class="col-sm-2 text-end control-label col-form-label">Tanggal Keluar</label>
             <div class="col-sm-9">
                 <div class="input-group ">
-                    <i class="bi-calendar3 input-group-text "></i>
-                    <input type="text" class="form-control" id="validationDefault01" value="<?= $anggota['TGLK_ANG']; ?>" required readonly>
-                    <br>
+                    <i class="bi-calendar3 input-group-text"></i>
+                    <input type="text" class="form-control" id="TGLK_ANG" placeholder="-" name="TGLK_ANG" value="<?= $anggota['TGLK_ANG']; ?>">
                 </div>
+                <small class="form-text text-danger"><?= form_error('TGLK_ANG') ?></small>
             </div>
         </div>
-        <div class="form-group row mb-2 ms-5">
-            <label for="Golongan" class="from-label">Golongan</label>
+
+        <div class="form-group row mb-2">
+            <label for="Golongan" class="col-sm-2 text-end control-label col-form-label">Golongan</label>
             <div class="col-sm-9">
                 <div class="input-group ">
                     <i class="bi-people-fill input-group-text"></i>
-                    <input type="text" class="form-control" id="validationDefault01" value="<?= $anggota['GOL']; ?>" required readonly>
-                    <br>
+                    <input type="text" class="form-control" id="GOL" placeholder="-" name="GOL" value="<?= $anggota['GOL']; ?>">
                 </div>
+                <small class="form-text text-danger"><?= form_error('GOL') ?></small>
             </div>
         </div>
+
+        <div class="col-sm-2 text-end mt-3 ">
+            <input type="button" class="btn btn-warning" value="Kembali" onclick="goBack()">
+        </div>
+>>>>>>> Stashed changes
     </div>
 </div>
+
+<script>
+    function goBack() {
+        window.history.back();
+    }
+</script>
+
 <?php
 $this->load->view('templates/footer');
 ?>
